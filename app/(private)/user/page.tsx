d8f3e7cbc804578ddb78a123f6a1e723ea03af8b
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2024-10-25 16:58:09 -0400

    bug: fetching todos based on userId and orgId

--- app/(private)/user/page.tsx
+++ app/(private)/user/page.tsx
@@ -8,40 +8,62 @@ import {
   getTodos,
   toggleTodo,
 } from '@/actions/todoActions'
+import { getOrgId } from '@/actions/orgActions'
 import { InsertTodo } from '@/drizzle/schema'
 import AddTodo from '@/components/AddTodo'
 import TodoView from '@/components/TodoView'
 
 function App() {
   const [todos, setTodos] = useState<InsertTodo[]>([])
-  const [newTodo, setNewTodo] = useState('')
+  const [orgId, setOrgId] = useState<string>('')
   const { userId } = useAuth()
+
+  // useEffect(() => {
+  //   const fetchOrgId_Todos = async () => {
+  //     if (userId != null) {
+  //       const orgId = await getOrgId(userId)
+  //       setOrgId(orgId)
+  //       const todos = await getTodos(userId, orgId)
+  //       setTodos(todos)
+  //     }
+  //   }
+  //   fetchOrgId_Todos()
+  // }, [])
+
+  useEffect(() => {
+    const fetchOrgId = async () => {
+      'use server'
+      if (userId != null) {
+        const orgId = await getOrgId(userId)
+        setOrgId(orgId)
+      }
+    }
+    fetchOrgId()
+  }, [userId]) // Run when userId changes
 
   useEffect(() => {
     const fetchTodos = async () => {
-      if (userId != null) {
-        const todos = await getTodos(userId)
+      'use server'
+      if (userId != null && orgId != null) {
+        const todos = await getTodos(userId, orgId)
         setTodos(todos)
       }
     }
     fetchTodos()
-  }, [])
+  }, [userId, orgId]) // Run when userId or orgId changes
 
-  if (userId == null) return RedirectToSignIn({ redirectUrl: '/sign-in' })
+  if (userId == null) return RedirectToSignIn({ redirectUrl: '/' })
 
-  const handleCreateTodo = () => {
-    if (newTodo.trim()) {
-      addTodo(userId, newTodo)
-      setTodos((prev) => [
-        { clerkUserId: userId, text: newTodo, isCompleted: false },
-        ...prev,
-      ])
-      setNewTodo('')
-    }
+  const handleCreateTodo = (newTodo: string) => {
+    addTodo(userId, orgId, newTodo)
+    setTodos((prev) => [
+      { clerkUserId: userId, orgId, text: newTodo, isCompleted: false },
+      ...prev,
+    ])
   }
 
   const handleToggle = (id: string) => {
-    toggleTodo(id, userId)
+    toggleTodo(id, userId, orgId)
     setTodos(
       todos.map((todo) =>
         todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
@@ -50,13 +72,14 @@ function App() {
   }
 
   const handleDelete = (id: string) => {
-    deleteTodo(id, userId)
+    deleteTodo(id, userId, orgId)
     setTodos(todos.filter((todo) => todo.id !== id))
   }
 
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Org id: {orgId} </h1>
         <TodoView
           todos={todos}
           handleToggle={handleToggle}
